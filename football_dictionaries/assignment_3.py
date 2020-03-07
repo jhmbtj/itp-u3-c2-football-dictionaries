f29a9e2ce52c613bf346c09064c49fe79ff25047
jhmbtj <60899943+jhmbtj@users.noreply.github.com>
2020-03-07 18:50:09 -0500

    Update assignment_3.py

--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,8 +1,8 @@
 from football_dictionaries.assignment_2 import players_by_position, player_to_dictionary
 def players_by_country_and_position(squads_list):
     import itertools
-    new_list = {}
-    for key, value in itertools.groupby(squads_list, key = lambda item: itme[6]):
-        new_list[key] = players_by_position(value)
-    return new_list
+    new_dictionary = {}
+    for key, group in itertools.groupby(squads_list, key = lambda item: itme[6]):
+        new_list[key] = players_by_position(group)
+    return new_dictionary
 
