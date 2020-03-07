f7567baaf9650580933bf86b11fb74a9d17ccb26
jhmbtj <60899943+jhmbtj@users.noreply.github.com>
2020-03-07 09:54:21 -0500

    Football dictionary assignment 2

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,19 @@
 def players_by_position(squads_list):
-    pass
+    new_list_by_position = {}
+    for player in squads_list:
+        position = player[1]
+        new_list_by_position.setdefault(position, [])
+        new_list_by_position[position].append({
+            'number': player[0],
+            'position': position,
+            'name': player[2],
+            'date_of_birth': player[3],
+            'caps': player[4],
+            'club': player[5],
+            'country': player[6],
+            'club_country': player[7],
+            'year': player[8]
+        })
+    return new_list_by_position
+        
+        
